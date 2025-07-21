fea16e5c065aee74709effd88d9acefa39751d08
Anton Averchenkov <anton.averchenkov@hashicorp.com>
2025-07-21 03:05:40 -0400

    fix test

--- internal/graph/print_test.go
+++ internal/graph/print_test.go
@@ -17,17 +17,17 @@ func TestPrintNode(t *testing.T) {
 
 	assert.Equal(
 		t,
-		`  my_service                 [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/blues8/7"  color = "/blues8/8"  fontcolor = "white"      fontsize = "8pt"  label = "my-service"];`+"\n",
+		`  my_service                           [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/blues8/7"  color = "/blues8/8"  fontcolor = "white"      fontsize = "8pt"  label = "my-service"];`+"\n",
 		b1.String(),
 	)
 	assert.Equal(
 		t,
-		`  cadence_service            [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/brbg8/7"   color = "/brbg8/8"   fontcolor = "white"      label = "cadence-service"];`+"\n",
+		`  cadence_service                      [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/brbg8/7"   color = "/brbg8/8"   fontcolor = "white"      label = "cadence-service"];`+"\n",
 		b2.String(),
 	)
 	assert.Equal(
 		t,
-		`    my_proxy                 [shape = "diamond"    style = "rounded,bold,filled"    fillcolor = "/bupu8/7"   color = "/bupu8/8"   fontcolor = "white"      fontsize = "8pt"  label = "my-proxy"];`+"\n",
+		`    my_proxy                           [shape = "diamond"    style = "rounded,bold,filled"    fillcolor = "/bupu8/7"   color = "/bupu8/8"   fontcolor = "white"      fontsize = "8pt"  label = "my-proxy"];`+"\n",
 		b3.String(),
 	)
 }
@@ -52,14 +52,13 @@ func TestPrintDependencies(t *testing.T) {
 			Condition: compose.ConditionServiceCompletedSuccessfully,
 		}},
 	)
-	t.Log(b.String())
 
 	assert.Contains(
 		t, `
-  my_service                 -> test_service_0             [style="bold"];
-  my_service                 -> test_service_1             [arrowhead="diamond" style="bold"];
-  my_service                 -> test_service_2             [style="dashed"];
-  my_service                 -> test_service_3             [arrowhead="diamond" style="bold"];
+  my_service                           -> test_service_2                       [style="dashed"];
+  my_service                           -> test_service_1                       [arrowhead="diamond" style="bold"];
+  my_service                           -> test_service_3                       [arrowhead="diamond" style="bold"];
+  my_service                           -> test_service_0                       [style="bold"];
 `,
 		b.String(),
 	)
