d18b2e50411417db99cf5771e5f032e13c6f264a
Anton Averchenkov <84287187+averche@users.noreply.github.com>
2025-07-21 07:06:23 +0000

    Set up go build workflow (#13)
    * Set up go build workflow
    
    * go version
    
    * test
    
    * fix test

--- internal/graph/print_test.go
+++ internal/graph/print_test.go
@@ -17,38 +17,18 @@ func TestPrintNode(t *testing.T) {
 
 	assert.Equal(
 		t,
-		`  my_service                 [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/blues8/7"  color = "/blues8/8"  fontcolor = "white"      fontsize = "8pt"  label = "my-service"];`+"\n",
+		`  my_service                           [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/blues8/7"  color = "/blues8/8"  fontcolor = "white"      fontsize = "8pt"  label = "my-service"];`+"\n",
 		b1.String(),
 	)
 	assert.Equal(
 		t,
-		`  cadence_service            [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/brbg8/7"   color = "/brbg8/8"   fontcolor = "white"      label = "cadence-service"];`+"\n",
+		`  cadence_service                      [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/brbg8/7"   color = "/brbg8/8"   fontcolor = "white"      label = "cadence-service"];`+"\n",
 		b2.String(),
 	)
 	assert.Equal(
 		t,
-		`    my_proxy                 [shape = "diamond"    style = "rounded,bold,filled"    fillcolor = "/bupu8/7"   color = "/bupu8/8"   fontcolor = "white"      fontsize = "8pt"  label = "my-proxy"];`+"\n",
+		`    my_proxy                           [shape = "diamond"    style = "rounded,bold,filled"    fillcolor = "/bupu8/7"   color = "/bupu8/8"   fontcolor = "white"      fontsize = "8pt"  label = "my-proxy"];`+"\n",
 		b3.String(),
-	)
-}
-
-func TestPrintNodeWithVolumes(t *testing.T) {
-	var b strings.Builder
-
-	printNodeWithVolumes(&b, "my-service", CategoryService, 5, []compose.VolumeMount{{Source: "./my/dir/", Target: "/target/dir/"}})
-
-	assert.Contains(
-		t, `
-  subgraph cluster_5 {
-      shape = "box"
-      style = "rounded,bold,dashed"
-      color = "/blues8/8"
-    my_service               [shape = "box"        style = "rounded,bold,filled"    fillcolor = "/blues8/7"  color = "/blues8/8"  fontcolor = "white"      label = "my-service"];
-    my_service_v0            [shape = "cylinder"   style = "rounded,bold,dashed"                             color = "/blues8/8"  fontcolor = "/greys8/8"  label = "volume\nfrom: ./my/dir/\nto: /target/dir/"];
-    my_service               -> my_service_v0;
-  }
-`,
-		b.String(),
 	)
 }
 
@@ -72,14 +52,13 @@ func TestPrintDependencies(t *testing.T) {
 			Condition: compose.ConditionServiceCompletedSuccessfully,
 		}},
 	)
-	t.Log(b.String())
 
 	assert.Contains(
 		t, `
-  my_service                 -> test_service_0             [style="bold"];
-  my_service                 -> test_service_1             [arrowhead="diamond" style="bold"];
-  my_service                 -> test_service_2             [style="dashed"];
-  my_service                 -> test_service_3             [arrowhead="diamond" style="bold"];
+  my_service                           -> test_service_2                       [style="dashed"];
+  my_service                           -> test_service_1                       [arrowhead="diamond" style="bold"];
+  my_service                           -> test_service_3                       [arrowhead="diamond" style="bold"];
+  my_service                           -> test_service_0                       [style="bold"];
 `,
 		b.String(),
 	)
